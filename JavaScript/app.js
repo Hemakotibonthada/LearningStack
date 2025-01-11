08cf5df7ad759df14fa4f1839328eadd269948e5
Tejasri <tejasriveeranki.113@gmail.com>
2025-01-11 21:00:32 +0530

    page

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -148,3 +148,17 @@ console.log("Division:", division);
 
 
 ////This is For Teja's Place////
+let marks=75;
+if(marks>=80){
+    console.log("Grade A");
+}
+ else if(marks>=60){
+    console.log("Grade B");
+}
+ else if(marks>=30){
+    console.log("Grade C");
+}
+ else{
+    console.log("Fail");
+}
+
