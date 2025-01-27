a13be0905590513f066fe00bad51c3af67d39042
Hemakoti Bonthada <Hemakotibonthada@gmail.com>
2025-01-27 20:09:25 +0530

    JS Practice

--- JavaScript/todo.js
+++ JavaScript/todo.js
@@ -42,100 +42,100 @@
 //     }
 // }
 
-const student={
-    name:"Hemakoti Bonthada",
-    age:25,
-    interests:['Coding','Reading','Writing','Sleeping'],
-    marks:78.85,
-    maritialStatus:"Married",
-    spouse:"Tejasri"
-};
+// const student={
+//     name:"Hemakoti Bonthada",
+//     age:25,
+//     interests:['Coding','Reading','Writing','Sleeping'],
+//     marks:78.85,
+//     maritialStatus:"Married",
+//     spouse:"Tejasri"
+// };
 
-const post={
-username:"Hemakoti.bonthada",
-content:"Hello World, Now I'm Free from Social Media and But This Is The Post Which I Want To Share About My Status.",
-comments:["Wow","Nice","Good","Superb","Excellent",'Pakkaki Poi Adukomma'],
-likes:1,
-shares:1,
-tags:["#NoSocialMedia","#NoFacebook","#NoInstagram","#NoTwitter"],
-location:"India",
-reposts:0
-};
+// const post={
+// username:"Hemakoti.bonthada",
+// content:"Hello World, Now I'm Free from Social Media and But This Is The Post Which I Want To Share About My Status.",
+// comments:["Wow","Nice","Good","Superb","Excellent",'Pakkaki Poi Adukomma'],
+// likes:1,
+// shares:1,
+// tags:["#NoSocialMedia","#NoFacebook","#NoInstagram","#NoTwitter"],
+// location:"India",
+// reposts:0
+// };
 
-// let maxnum=prompt("Please Enter a Maximum Number or to Quit the Game Enter quit or q: ");
-// //maxnum=parseInt(maxnum);
-// let rnum=Math.floor(Math.random()*maxnum)+1;
-// while(maxnum!='q'){
-//     let Guess=prompt("Please Guess The Number and Enter");
-//     //Guess=parseInt(Guess);
-//     if (rnum==Guess){
-//         console.log("Congrats You Guessed The Correct Number");
-//         break;
-//     }
-//     else if(Guess=="quit" || Guess=="q"){
-//         break;
-//     }
-//     else{
-//         console.log("Your Guess the Wrong number please try again");
-//     }
+// // let maxnum=prompt("Please Enter a Maximum Number or to Quit the Game Enter quit or q: ");
+// // //maxnum=parseInt(maxnum);
+// // let rnum=Math.floor(Math.random()*maxnum)+1;
+// // while(maxnum!='q'){
+// //     let Guess=prompt("Please Guess The Number and Enter");
+// //     //Guess=parseInt(Guess);
+// //     if (rnum==Guess){
+// //         console.log("Congrats You Guessed The Correct Number");
+// //         break;
+// //     }
+// //     else if(Guess=="quit" || Guess=="q"){
+// //         break;
+// //     }
+// //     else{
+// //         console.log("Your Guess the Wrong number please try again");
+// //     }
     
-// }
+// // }
 
 
-// function poem(k){
-//     if (k=="jony"){
-//         console.log("Johny Johny Yes Papa");
-//         console.log("Eating Sugar No Papa");
-//         console.log("Telling Lies No Papa");
-//         console.log("Open Your Mouth Ha Ha Ha");
-//     }
-//     else if(k=="twinkle"){
-//         console.log("Twinkle Twinkle Little Star");
-//         console.log("How I Wonder What You Are");
-//         console.log("Up Above The World So High");
-//         console.log("Like A Diamond In The Sky");
+// // function poem(k){
+// //     if (k=="jony"){
+// //         console.log("Johny Johny Yes Papa");
+// //         console.log("Eating Sugar No Papa");
+// //         console.log("Telling Lies No Papa");
+// //         console.log("Open Your Mouth Ha Ha Ha");
+// //     }
+// //     else if(k=="twinkle"){
+// //         console.log("Twinkle Twinkle Little Star");
+// //         console.log("How I Wonder What You Are");
+// //         console.log("Up Above The World So High");
+// //         console.log("Like A Diamond In The Sky");
+// //     }
+// //     else{
+// //         console.log("Invalid Input");
+// //     }
+
+// // }
+// // let n=prompt("Please Enter a Poem Name");
+// // poem(n);
+// let majors={};
+// let minors={};
+// let count=0;
+// id();
+// createuser();
+
+// function isadult(age){
+//     if(age>=18){
+//         return true;
 //     }
 //     else{
-//         console.log("Invalid Input");
+//         return false;
 //     }
-
 // }
-// let n=prompt("Please Enter a Poem Name");
-// poem(n);
-let majors={};
-let minors={};
-let count=0;
-id();
-createuser();
+// function id(count){
+//     let newusercount=`user${count}`;
+//     return newusercount;
+// }
 
-function isadult(age){
-    if(age>=18){
-        return true;
-    }
-    else{
-        return false;
-    }
-}
-function id(count){
-    let newusercount=`user${count}`;
-    return newusercount;
-}
-
-function createuser(){
-        let uname=prompt("Please Enter your name: ");
-        let age=prompt("Enter your age : ");
-        if (isadult(age)){
-            console.log("User is eligible to have access.");
-            majors[id(count)]={name:uname,age:age};
-            console.log(majors);
-            count++;
-        }else{
-            console.log("user not eligible to have access.");
-            minors[id(count)]={name:uname,age:age};
-            console.log(minors);
-            count++;
-        }
-}
+// function createuser(){
+//         let uname=prompt("Please Enter your name: ");
+//         let age=prompt("Enter your age : ");
+//         if (isadult(age)){
+//             console.log("User is eligible to have access.");
+//             majors[id(count)]={name:uname,age:age};
+//             console.log(majors);
+//             count++;
+//         }else{
+//             console.log("user not eligible to have access.");
+//             minors[id(count)]={name:uname,age:age};
+//             console.log(minors);
+//             count++;
+//         }
+// }
 // let uname=prompt("Please Enter your name: ");
 // let age=prompt("Enter your age : ");
 // id();
@@ -147,3 +147,72 @@ function createuser(){
 // }else{
 //     console.log("user not eligible to have access.");
 // }
+
+
+// function MultiplicationTable(num){
+//     for(let i=1;i<=20;i++){
+//         console.log(`${num} * ${i} = ${num*i}`);
+//     }
+// }
+// let num=prompt("Please Enter the Table Number that you wanna see: ");
+// MultiplicationTable(num);
+
+
+// function sumofPrime(n){
+//     let sum=0;
+//     for (let i=1;i<=n;i++){
+//         sum+=i;
+//     }
+//     return sum;
+// }
+
+// let n=prompt("Please Enter the Number That You Wanna Know The Prime Sum: ");
+// console.log(sumofPrime(n));
+
+// const Hema=function(a,b){
+// console.log("Hemakoti Bonthada Here.!"+ a*b);
+// }
+// Hema(10,20);
+// function Hemakoti(a,b){
+//     for(let i=0;i<b;i++){
+//         a(i);
+//     }
+// }
+// const a=function(i){
+//     console.log(`This is a High order Function and it's going to be run on n Times and the value of n is ${i} for this time`);
+// }
+
+// let b=prompt("Please enter how many times function needs to be run!")
+// Hemakoti(a,b);
+
+// function oddeventest(req){
+//     if (req=="odd"){
+//         return function(n){
+//             console.log(!((n%2)==0));
+//         }
+//     }
+//     else if(req=="even"){
+//         return function(n){
+//             console.log((n%2)==0);
+//         }
+//     }
+//     else{
+//         console("Please Enter proper input which is Either even or odd only.");
+//     }
+// }
+
+const calculator=
+{
+ add:function(a,b){
+    return a+b;
+ },
+ sub:function(a,b){
+    return a-b;
+ },
+ mul:function(a,b){
+    return a*b;
+ },
+ div:function(a,b){
+    return a/b;
+ }
+}
